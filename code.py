f8f50cf25a62bf302f07e7ee407702f1267679ac
Raiam-Dev <raianpipaarranha@gmail.com>
2025-04-05 21:42:08 -0300

    Primeiro Teste

---
+++ code.py
@@ -0,0 +1,8 @@
+def funcao (a):
+    print(a)
+    a = (a + a) * a
+    print(a)
+
+if __name__ == "__code__":
+    a = 10
+    funcao(a)
---
+++ main.py
@@ -0,0 +1,8 @@
+def funcao (a):
+    print(a)
+    a = (a + a) * a
+    print(a)
+
+if __name__ == "__main__":
+    a = 10
+    funcao(a)
